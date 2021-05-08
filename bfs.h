d46fc9d1b4bc588132f1137a1acd0b2401dca5c8
srisatyavinay <81536213+srisatyavinay@users.noreply.github.com>
2021-05-08 17:28:10 +0530

    Update bfs.h

--- bfs.h
+++ bfs.h
@@ -1,24 +1,12 @@
 #ifndef __BFS_H
 #define __BFS_H
 
+#include "graph.h"
+
 typedef struct Queue StrQueue;
 typedef StrQueue* PtrQueue;
 typedef struct QueueNode StrQueueNode;
 typedef StrQueueNode* PtrQueueNode;
-typedef struct Graph StrGraph;
-typedef StrGraph* PtrGraph;
-typedef struct GraphNode StrGraphNode;
-typedef StrGraphNode* PtrGraphNode;
-
-struct Graph {
-    int vertices;
-    PtrGraphNode* list;
-};
-
-struct GraphNode {
-    int vertex;
-    PtrGraphNode next;
-};
 
 struct QueueNode
 {
@@ -37,9 +25,6 @@ PtrQueue CreateQueue();
 void Enqueue(PtrQueue q, int element);
 int Dequeue(PtrQueue q);
 int IsEmpty(PtrQueue q);
-void bfs(struct Graph* g, int begin);
-PtrGraphNode NewGraphNode(int vertex);
-PtrGraph CreateGraph(int num);
-void InsertEdge(PtrGraph g, int start, int end);
+void bfs(Graph* g, int begin);
 
 #endif
