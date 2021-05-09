f8ba2a24a4d59a4ee7e0686a4e8378dd60f606f3
Random <71204119+Ag-Shivangi@users.noreply.github.com>
2021-05-09 20:06:16 +0530

    Added case 3

--- main.c
+++ main.c
@@ -3,15 +3,14 @@
 int main()
 {
     initialise(); //all initalisation goes here    
-    int n = 256, user_choice, user_count = 0;
-    int uid; //n=size of the user array(preferably a power of 2)
+    int n = 256, user_choice, user_count = 0; //n=size of the user array(preferably a power of 2)
     Graph graph = CreateGraph(n);
     printf("\n  BONJOUR! WELCOME TO THE FRIENDS RECCOMENDATION SYSTEM \n\n");
     do
     {
         printf("********************************************************\n\n");
         printf("\t      *********************\n\t\tEnter your choice\n\t      *********************\n");
-        printf("\t1)\tRegister new user\n\t2)\tLogin to your Profile\n\t3)\tExit Program\n\n");
+        printf("\t1)\tRegister new user\n\t2)\tLogin to your Profile\n\t3)\tCheck Friendship Status\n\t4)\tExit Program\n\n");
         printf("********************************************************\n");
         scanf("%d", &user_choice);
         switch (user_choice)
@@ -21,17 +20,17 @@ int main()
             create_user(graph);
             break;
         case 2:            
-            // user_login(graph);
+            user_login(graph);
             break;
         case 3:
+            frienship_status(graph);
             break;
         case 4:
-            scanf("%d", &uid);
-            display_details(uid);
+            break;
         default:
             printf("\n Time to visit a Optometrist? Or some Coffee?\n\n");
             break;
         }
-    } while (user_choice != 3);
+    } while (user_choice != 4);
     printf("\n\t****  Au Revoir Mon Amie :)  ****\n");
 }
